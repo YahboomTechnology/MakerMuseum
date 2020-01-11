faa692d11ded221c41f5b4fd70b55f3b94e759e4
chengy17 <chengy_95@163.com>
2020-01-11 17:57:53 +0800

    add MakerMuseum

---
+++ main.ts
@@ -0,0 +1,11 @@
+/*
+Copyright (C): 2020, Shenzhen Yahboom Tech
+Edited By chengengyue
+
+"MakerMuseum": "file:../MakerMuseum"
+*/
+
+//% color="#ECA40D" weight=26 icon="\uf1b2"
+namespace MakerMuseum {
+
+}
